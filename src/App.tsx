1ef6f228856deca9d36769dab6ea07da736b1dce
Abhishek N <abhishekpes123@gmail.com>
2025-12-19 20:54:46 +0530

    chnaged app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 import MainLayout from "./components/Layout/AppLayout";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
