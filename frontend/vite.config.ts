25d21353b736f0e6a50fbf281fb2bc5e4977f887
Abhishek N <abhishekpes123@gmail.com>
2025-12-24 09:57:44 +0530

    remove vercel.json and comment out build configuration in vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build' // default is 'dist'
-  }
+  // build: {
+  //   outDir: 'build' // default is 'dist'
+  // }
 })
 
